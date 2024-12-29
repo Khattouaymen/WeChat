5c22f9acf21932a664e601c153f0ab45b76cea2a
AYMEN KHATTOU <122833986+Khattouaymen@users.noreply.github.com>
2024-12-30 00:46:12 +0100

    1.0

---
+++ app/src/main/java/com/example/wechat/utils/AndroidUtil.java
@@ -0,0 +1,34 @@
+package com.example.wechat.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.wechat.model.UserModel;
+
+public class AndroidUtil {
+
+    public static void showToast(Context context, String message){
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+    public static void passUserModelAsIntent(Intent intent, UserModel userModel){
+        intent.putExtra("username",userModel.getUsername());
+        intent.putExtra("phone",userModel.getPhone());
+        intent.putExtra("userId",userModel.getUserId());
+    }
+    public static UserModel getUserModelFromIntent(Intent intent){
+        UserModel userModel = new UserModel();
+        userModel.setUsername(intent.getStringExtra("username"));
+        userModel.setPhone(intent.getStringExtra("phone"));
+        userModel.setUserId(intent.getStringExtra("userId"));
+        return userModel;
+    }
+    public static void setProfilePic(Context context, Uri imageUri, ImageView imageView){
+        Glide.with(context).load(imageUri).apply(RequestOptions.circleCropTransform()).into(imageView);
+    }
+}
